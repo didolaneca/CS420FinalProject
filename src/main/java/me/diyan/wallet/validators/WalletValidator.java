cfa53befde695f6e372e0527b0ebfca9fc79bf34
Diyan Simeonov <diyan_simeonov@yahoo.com>
2021-05-04 11:02:34 -0500

    Added an implementation to the Date validator method

--- src/main/java/me/diyan/wallet/validators/WalletValidator.java
+++ src/main/java/me/diyan/wallet/validators/WalletValidator.java
@@ -17,7 +17,33 @@ public class WalletValidator implements AmountValidator, DateValidator{
 
     @Override
     public boolean validateDate(TextField textField) {
-        String amount = textField.getText();
+        String date = textField.getText();
+        int month, day, year;
+        if(date.matches("\\d{2}/\\d{2}/\\d{4}")){
+            try{
+                month = Integer.parseInt(date.substring(0, 2));
+                day = Integer.parseInt(date.substring(3, 5));
+                year = Integer.parseInt(date.substring(6));
+                if(month < 13 && month > 0){
+                    if(month == 2 && day < 29){
+                        return true;
+                    } else if((month == 1 || month == 3 || month == 5 || month == 7
+                                || month == 8 || month == 10 || month == 12) && day < 32){
+                        return true;
+                    } else if((month == 4 || month == 6 || month == 9 || month == 11)
+                            && day < 31){
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            } catch (NumberFormatException ex ){
+                System.out.println("Error! Date not in proper format -> " + ex.getMessage());
+                return false;
+
+            }
+//            return true;
+        }
         return false;
     }
 }
