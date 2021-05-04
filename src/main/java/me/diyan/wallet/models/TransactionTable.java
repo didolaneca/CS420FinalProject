9c238de272f6c5dcda8f1445e63418387637fb77
Diyan Simeonov <diyan_simeonov@yahoo.com>
2021-05-04 11:05:41 -0500

    Added a TransactionTable class to generate the tableView with the transactions

--- src/main/java/me/diyan/wallet/models/TransactionTable.java
+++ src/main/java/me/diyan/wallet/models/TransactionTable.java
@@ -11,26 +11,25 @@ import java.util.Date;
 public class TransactionTable {
     public static TableView<Transaction> createTransactionTable(ObservableList<Transaction> transactions){
         TableView<Transaction> transactionTableView = new TableView<>();
-//        if (transactions.isEmpty()) {
-//            transactionTableView.setPlaceholder(new Label("No rows to display!"));
-//            return transactionTableView;
-//        }
-//        TableColumn<Transaction, Date> dateColumn = new TableColumn<>("Date");
-//        dateColumn.setMinWidth(30);
-//        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-//
-//        TableColumn<Transaction, Double> amountColumn = new TableColumn<>("Amount");
-//        dateColumn.setMinWidth(30);
-//        dateColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
-//
-//        TableColumn<Transaction, String> noteColumn = new TableColumn<>("Note");
-//        dateColumn.setMinWidth(100);
-//        dateColumn.setCellValueFactory(new PropertyValueFactory<>("note"));
-//
-//        transactionTableView.setItems(transactions);
-//        transactionTableView.getColumns().addAll(dateColumn, amountColumn, noteColumn);
+        TableColumn<Transaction, Date> dateColumn = new TableColumn<>("Date");
+        dateColumn.setMinWidth(100);
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
 
-        transactionTableView.setPlaceholder(new Label("No rows to display!"));
+        TableColumn<Transaction, Double> amountColumn = new TableColumn<>("Amount");
+        amountColumn.setMinWidth(100);
+        amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
+
+        TableColumn<Transaction, String> noteColumn = new TableColumn<>("Note");
+        noteColumn.setMinWidth(250);
+        noteColumn.setCellValueFactory(new PropertyValueFactory<>("note"));
+
+        if (transactions.isEmpty()) {
+            transactionTableView.setPlaceholder(new Label("No rows to display!"));
+            return transactionTableView;
+        }
+
+        transactionTableView.setItems(transactions);
+        transactionTableView.getColumns().addAll(dateColumn, amountColumn, noteColumn);
         return transactionTableView;
     }
 }
